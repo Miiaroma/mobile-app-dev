5a6ab412d5866419eb1143727fae5b83c8fb39fd
Miiaroma <miia.romanainen@gmail.com>
2021-01-11 13:54:37 +0200

    Row view
    Row view created.

---
+++ landmark/landmark/Shared/Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  landmark (iOS)
+//
+//  Created by Miia Romanainen on 11.1.2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()                }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
