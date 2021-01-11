ba2e1be76c3ac26c08996f1c6e038c38e488118f
Miiaroma <miia.romanainen@gmail.com>
2021-01-11 15:20:42 +0200

    Map and Detail view
    Map and detail view done

---
+++ landmark/landmark/Shared/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  landmark
+//
+//  Created by Miia Romanainen on 11.1.2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ landmark/landmark/Shared/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  landmark
+//
+//  Created by Miia Romanainen on 11.1.2021.
+//
+
+import Foundation
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
