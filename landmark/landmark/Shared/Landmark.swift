15045a4329b7629ae20ff0fb55978997d747bb86
Miiaroma <miia.romanainen@gmail.com>
2021-01-11 13:24:11 +0200

    Building list and navigation
    Building list and navigation added.

---
+++ landmark/landmark/Shared/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  landmark
+//
+//  Created by Miia Romanainen on 11.1.2021.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable
+{
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+        var image: Image {
+            Image(imageName)
+        }
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+            CLLocationCoordinate2D(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude)
+        }
+        struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
+    
+}
