6c35896b2fe3d57e002200170e543f6b836ced1c
Miiaroma <miia.romanainen@gmail.com>
2021-01-11 10:39:42 +0200

    Mobile Applications
    Project initialized.

---
+++ landmark/landmark/Shared/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  Shared
+//
+//  Created by Miia Romanainen on 11.1.2021.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
