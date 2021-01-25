d7c08daed020e4f9c75a456bdf3d95174eccd8ff
Miiaroma <miia.romanainen@gmail.com>
2021-01-25 14:30:00 +0200

    WeatherApp
    Project initialized.

---
+++ WeatherApp/Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Miia Romanainen on 25.1.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
