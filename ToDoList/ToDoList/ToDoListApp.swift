0d98abe1ccc6660f0dee490e3923726dec5583fc
Miiaroma <miia.romanainen@gmail.com>
2021-02-08 14:40:09 +0200

    ToDoList initialized
    Project initialized

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Miia Romanainen on 8.2.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
