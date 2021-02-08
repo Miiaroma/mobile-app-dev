6186f5fcb6c485d482c883464cd89ab883bc0b03
Miiaroma <miia.romanainen@gmail.com>
2021-02-08 15:31:51 +0200

    Content view modified
    Content view modifications

--- ToDoList/ToDoList/ContentView.swift
+++ ToDoList/ToDoList/ContentView.swift
@@ -7,10 +7,24 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List{
+                ForEach(menu){ section in Text(section.name)
+                    
+                    ForEach(section.items) { item in Text(item.name)
+                    }
+                }
+            }
+            .navigationBarTitle("Menu")
+            .listStyle(GroupedListStyle())        }
+        
+        .padding()
     }
 }
 
