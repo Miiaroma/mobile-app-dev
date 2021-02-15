2deb73391701aa928ffb0b1cd5433889e1f5104c
Miiaroma <miia.romanainen@gmail.com>
2021-02-15 09:58:59 +0200

    Menu list done
    Building a menu using list done.

--- ToDoList/ToDoList/ContentView.swift
+++ ToDoList/ToDoList/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 
-
 struct ContentView: View {
     let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
     
     var body: some View {
         NavigationView {
-            List{
-                ForEach(menu){ section in Text(section.name)
-                    
-                    ForEach(section.items) { item in Text(item.name)
+            List {
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)){
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
                     }
                 }
             }
             .navigationBarTitle("Menu")
-            .listStyle(GroupedListStyle())        }
-        
-        .padding()
+            .listStyle(GroupedListStyle())
+        }
     }
 }
 
