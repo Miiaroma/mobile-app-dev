33fa28f0f1be3d8cadcf3a28119f721a79153101
Miiaroma <miia.romanainen@gmail.com>
2021-02-15 15:01:16 +0200

    An alert added.

--- ToDoList/ToDoList/CheckoutView.swift
+++ ToDoList/ToDoList/CheckoutView.swift
@@ -13,9 +13,20 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
     
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     let tipAmounts = [10, 15, 20, 25, 0]
+    
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
+    }
     
     var body: some View {
         Form {
@@ -40,14 +51,17 @@ struct CheckoutView: View {
                 .pickerStyle(SegmentedPickerStyle())
             }
             Section(header:
-                        Text("TOTAL: $100")
+                        Text("TOTAL: \(totalPrice)")
             ) {
                 Button("Confirm order") {
-                    // place the order
+                    showingPaymentAlert.toggle()
                 }
             }        }
             .navigationTitle("Payment")
             .navigationBarTitleDisplayMode(.inline)
+            .alert(isPresented: $showingPaymentAlert) {
+                Alert(title: Text("Order confirmed"), message: Text("Your total was \(totalPrice) – thank you!"), dismissButton: .default(Text("OK")))
+            }
     }
 }
 
