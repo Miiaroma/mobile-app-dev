37279685fbb072805224ded5fcef89100057a328
Miiaroma <miia.romanainen@gmail.com>
2021-02-15 14:38:37 +0200

    CheckoutView bindings done.

--- ToDoList/ToDoList/OrderView.swift
+++ ToDoList/ToDoList/OrderView.swift
@@ -23,7 +23,7 @@ struct OrderView: View {
                 }
                 Section {
                     NavigationLink(destination:
-                        Text("Check out")) {
+                        CheckoutView()) {
                         Text("Place Order")
                     }
                 }
