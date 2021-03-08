15eee9cd4928afd94dd228a00737e7958928a46a
Miiaroma <miia.romanainen@gmail.com>
2021-03-08 14:08:03 +0200

    Project initialized

---
+++ Camera/Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Miia Romanainen on 8.3.2021.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
