ca11d4ae82aa969a746c52bf40ef53190e3ebc24
Miiaroma <miia.romanainen@gmail.com>
2021-03-08 09:00:05 +0200

    CoreDataToDo initialized

---
+++ CoreDataToDo/CoreDataToDo/ToDoItem.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoItem.swift
+//  CoreDataToDo
+//
+//  Created by Miia Romanainen on 8.3.2021.
+//
+
+import Foundation
+import CoreData
+
+public class ToDoItem: NSManagedObject, Identifiable{
+    @NSManaged public var createdAt:Date?
+    @NSManaged public var title:String?
+}
+
+extension ToDoItem {
+    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
+        let request:NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest()as!
+        NSFetchRequest<ToDoItem>
+        
+        let sortDesriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+    
+}
