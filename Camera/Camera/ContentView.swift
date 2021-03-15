f2c544bf34231320b3801043da46c6580d8f0634
Miiaroma <miia.romanainen@gmail.com>
2021-03-15 10:42:50 +0200

    CameraApp finalized

--- Camera/Camera/ContentView.swift
+++ Camera/Camera/ContentView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showImagePicker:Bool = false
+    @State var showActionSheet:Bool = false
+    @State var image:Image?
+    @State var sourceType:Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        ZStack {
+                VStack {
+                          image?
+                              .resizable()
+                              .clipped()
+                              .clipShape(Circle())
+                              .aspectRatio(contentMode: .fill)
+                              .frame(width: 160, height: 160)
+                            Text("Photo")
+                              .overlay(
+                                  CameraButtonView(showActionSheet: $showActionSheet)
+                                      .offset(x: 50, y: 65)
+                          )
+                      }.offset(y: -70)
+                      Spacer()
+    
+                  .actionSheet(isPresented: $showActionSheet, content: { () -> ActionSheet in
+                      ActionSheet(title: Text("Select Image"), message: Text("Please select photo from photo gallery or use the camera"), buttons: [
+                          ActionSheet.Button.default(Text("Camera"), action: {
+                              self.sourceType = 0
+                              self.showImagePicker.toggle()
+                          }),
+                        ActionSheet.Button.default(Text("Photo Gallery"), action: {
+                            self.sourceType = 1
+                            self.showImagePicker.toggle()
+                        }),
+                        ActionSheet.Button.cancel()
+                      ])
+                  })
+                  if showImagePicker {
+                    ImagePicker(isVisible: $showImagePicker, image: $image, sourceType: sourceType)
+                  }
+              }
+          }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- Camera/Camera/ImagePicker.swift
+++ Camera/Camera/ImagePicker.swift
@@ -11,16 +11,20 @@ struct ImagePicker: UIViewControllerRepresentable {
     
     @Binding var isVisible:Bool
     @Binding var image: Image?
-    var sourceType:Int
+    var sourceType: Int //UIImagePickerController.SourceType = .camera
     
     func makeUIViewController(context: Context) -> some UIImagePickerController {
         let vc = UIImagePickerController()
         vc.allowsEditing = true
-        vc.sourceType = sourceType == 1? .photoLibrary : .camera
+        vc.sourceType = sourceType == 1 ? .photoLibrary: .camera
         
         vc.delegate = context.coordinator
         return vc
     }
+    func makeCoordinator() -> Coordinator {
+        Coordinator(isVisible: $isVisible, image: $image)
+    }
+    
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         
     }
@@ -36,7 +40,7 @@ struct ImagePicker: UIViewControllerRepresentable {
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             let uiimage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-            image = Image(uiimage: uiimage)
+            image = Image(uiImage: uiimage)
             isVisible = false
         }
         
@@ -45,9 +49,3 @@ struct ImagePicker: UIViewControllerRepresentable {
         }
     }
 }
-
-/*struct ImagePicker_Previews: PreviewProvider {
-    static var previews: some View {
-        ImagePicker()
-    }
-}*/
